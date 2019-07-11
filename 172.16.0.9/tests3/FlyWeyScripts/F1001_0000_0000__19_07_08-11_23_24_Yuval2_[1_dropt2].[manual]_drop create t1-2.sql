8f451faeea201e25b28ebf591841b63d0849b7cd
Deployment <yperelman@hotmail.com>
2019-07-11 11:12:42 -0400

    auto commit after deployment

---
+++ 172.16.0.9/tests3/FlyWeyScripts/F1001_0000_0000__19_07_08-11_23_24_Yuval2_[1_dropt2].[manual]_drop create t1-2.sql
@@ -0,0 +1,14 @@
+------------------------Add row to flyway-schema-version-----------------
+
+IF EXISTS(SELECT TOP 1 1 FROM flyway_schema_history where [version] = '1001.0000.0000' OR description = '19 07 08-11 23 24 Yuval2 [1 dropt2].[manual] drop create t1-2' OR script = 'F1001_0000_0000__19_07_08-11_23_24_Yuval2_[1_dropt2].[manual]_drop create t1-2.sql')
+BEGIN
+	RAISERROR('The script "F1001_0000_0000__19_07_08-11_23_24_Yuval2_[1_dropt2].[manual]_drop create t1-2.sql" already ran on the database!',16,0)
+	GOTO EndScript
+END
+INSERT INTO flyway_schema_history(installed_rank,version,description,type,script,checksum,installed_by,
+installed_on,execution_time,success)
+VALUES((SELECT ISNULL( MAX(installed_rank),0) + 1 installed_rank FROM flyway_schema_history), '1001.0000.0000', '19 07 08-11 23 24 Yuval2 [1 dropt2].[manual] drop create t1-2', 'SQL','F1001_0000_0000__19_07_08-11_23_24_Yuval2_[1_dropt2].[manual]_drop create t1-2.sql', NULL, (SELECT  SUSER_SNAME()), (SELECT GETDATE()), 0,1 )
+-------------------------------------------------------------------------
+drop table t2
+-------------------------------------------------------------------------
+EndScript:
---
+++ 172.16.0.9/tests3/FlyWeyScripts/F1001_0000_0060__19_07_08-15_02_30_Yuval2_[SQLQuery7].[manual]_drop dv table-7.sql
@@ -0,0 +1,14 @@
+------------------------Add row to flyway-schema-version-----------------
+
+IF EXISTS(SELECT TOP 1 1 FROM flyway_schema_history where [version] = '1001.0000.0060' OR description = '19 07 08-15 02 30 Yuval2 [SQLQuery7].[manual] drop dv table-7' OR script = 'F1001_0000_0060__19_07_08-15_02_30_Yuval2_[SQLQuery7].[manual]_drop dv table-7.sql')
+BEGIN
+	RAISERROR('The script "F1001_0000_0060__19_07_08-15_02_30_Yuval2_[SQLQuery7].[manual]_drop dv table-7.sql" already ran on the database!',16,0)
+	GOTO EndScript
+END
+INSERT INTO flyway_schema_history(installed_rank,version,description,type,script,checksum,installed_by,
+installed_on,execution_time,success)
+VALUES((SELECT ISNULL( MAX(installed_rank),0) + 1 installed_rank FROM flyway_schema_history), '1001.0000.0060', '19 07 08-15 02 30 Yuval2 [SQLQuery7].[manual] drop dv table-7', 'SQL','F1001_0000_0060__19_07_08-15_02_30_Yuval2_[SQLQuery7].[manual]_drop dv table-7.sql', NULL, (SELECT  SUSER_SNAME()), (SELECT GETDATE()), 0,1 )
+-------------------------------------------------------------------------
+drop table dv
+-------------------------------------------------------------------------
+EndScript:
